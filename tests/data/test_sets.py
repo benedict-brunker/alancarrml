b3acb9ebed242b60434fdfc9f78a67363b163a74
Benedict Brunker <benedictbrunker@gmail.com>
2025-08-07 20:18:54 +1000

    setting up directory structure

---
+++ tests/data/test_sets.py
@@ -0,0 +1,10 @@
+# test_sets.py 
+def test_pop_target(): 
+    """
+    Unit tests for sets.pop() 
+    """ 
+    from alancarrml.data.sets import pop_target 
+
+    # Test that function returns the right datatypes 
+    
+    assert 
