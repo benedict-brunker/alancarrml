09dcc5cb1167cb6c83e0540e7fa0790032979a5e
Benedict Brunker <benedictbrunker@gmail.com>
2025-08-07 22:55:42 +1000

    fixed unit tests

--- tests/data/test_sets.py
+++ tests/data/test_sets.py
@@ -1,19 +1,63 @@
 # test_sets.py 
 
-# toy dataset for running tests 
-from sklearn.datasets import load_iris 
+# toy dataset for running tests  
 import pandas as pd 
 import numpy as np 
-import pytest
+import pytest 
+
+from sklearn.datasets import load_iris 
+
+from alancarrml.data.sets import pop_target 
 
 test_df = load_iris(as_frame=True).frame
+
+    # === Solution Unit Tests ===  # 
+
+# creating dummy data with pytest fixtures 
+@pytest.fixture
+def features_fixture(): 
+    features_data = [
+        [1, 25, "Junior"], 
+        [2, 33, "Confirmed"], 
+        [3, 42, "Manager"] 
+    ] 
+    return pd.DataFrame(features_data, columns=["employee_id", "age", "level"]) 
+
+# the @pytest.fixture decorator associates some value with the value returned by the function below the decorator 
+# e.g. in later tests below, target_fixture is always associated with the return value of the target_fixture() function 
+@pytest.fixture 
+def target_fixture(): 
+    target_data = [5, 10, 20] 
+    return pd.Series(target_data, name="salary", copy=False) 
+
+def test_pop_target_with_data_fixture(features_fixture, target_fixture): 
+    input_df = features_fixture.copy() 
+    input_df["salary"] = target_fixture 
+
+    features, target = pop_target(df=input_df, target_col='salary') 
+
+    pd.testing.assert_frame_equal(features, features_fixture) 
+    pd.testing.assert_series_equal(target, target_fixture) 
+
+def test_pop_target_no_col_found(features_fixture, target_fixture): 
+    input_df = features_fixture.copy() 
+
+    with pytest.raises(TypeError): 
+        features, target = pop_target(df=input_df, target_col=None) 
+
+def test_pop_target_df_none(features_fixture, target_fixture): 
+    input_df = features_fixture.copy() 
+
+    with pytest.raises(TypeError): 
+        features, target = pop_target(df=None, target_col='salary') 
+
+
+    # === Original attempt at unit tests ===  # 
 
 def test_pop_target(): 
     """
     Unit tests for sets.pop() 
     """ 
-    from alancarrml.data.sets import pop_target 
-
     # == Valid Inputs == 
 
     # Use function with valid inputs   
@@ -48,23 +92,23 @@ def test_pop_target():
     # -- ValueErrors -- 
 
     # Function rejects a series for df parameter 
-    with pytest.raises(ValueError):  
+    with pytest.raises(TypeError):  
         pop_target(X_series, 'target')  
 
     # Function rejects a dict for df parameter 
-    with pytest.raises(ValueError): 
+    with pytest.raises(TypeError): 
         pop_target(df_dict, 'target')   
 
     # Rejects numpy input 
-    with pytest.raises(ValueError): 
+    with pytest.raises(TypeError): 
         pop_target(df_np, 'target') 
 
     # Function rejects non-string parameter for target_col 
-    with pytest.raises(ValueError): 
+    with pytest.raises(TypeError): 
         pop_target(test_df, 35) 
     
     # Function rejects target_col not referencing column name in df 
-    with pytest.raises(ValueError): 
+    with pytest.raises(KeyError): 
         pop_target(test_df, 'flower_type')
 
     
